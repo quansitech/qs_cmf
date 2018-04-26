954a17b864799beb09f27ffe10e75284c037d7fe
tider <tider@quansitech.com>
2018-04-26 15:00:35 +0800

    DBCont添加匿名静态处理函数，以后添加常量不需要再新增读取方法

--- app/Gy_Library/DBCont.class.php
+++ app/Gy_Library/DBCont.class.php
@@ -489,12 +489,29 @@ class DBCont{
     static function getPublishStatusList(){
         return self::$_publish_status;
     }
-    
-    static function getContributeModelList(){
-        return array(
-            'EvshowCate',
-            'EvfriendCate'
-        );
+
+    static function __callStatic($name, $arguments)
+    {
+        $getListFn = function($var_name){
+            return self::$$var_name;
+        };
+
+        $getListValueFn = function($var_name) use ($arguments){
+
+            return (self::$$var_name)[$arguments[0]];
+        };
+
+        $static_name = '_';
+        if(preg_match("/get(\w+)List/", $name, $matches)){
+            $static_name .= parse_name($matches[1]);
+            $fn = $getListFn;
+        }
+        elseif(preg_match("/get(\w+)/", $name, $matches)){
+            $static_name .= parse_name($matches[1]);
+            $fn = $getListValueFn;
+        }
+
+        return $fn($static_name);
     }
     
 }
