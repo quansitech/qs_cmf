7c28b7a35bb5d991ef09fd0035ac4dc3fde4eb51
xhiny <axhiny@163.com>
2019-01-08 15:40:26 +0800

    Listbuilder添加区域筛选组件

--- app/Api/Controller/AreaController.class.php
+++ app/Api/Controller/AreaController.class.php
@@ -9,4 +9,28 @@ class AreaController extends Controller{
         $area_ents = $area->select();
         echo json_encode($area_ents);
     }
+
+    public function getProvince(){
+        $map['level'] = 1;
+
+        $province_list = D('Area')->where($map)->field('id,cname')->select();
+        $this->ajaxReturn($province_list);
+    }
+
+    public function getCityByProvince($province_id){
+        $map['upid'] = $province_id;
+        $map['level'] = 2;
+
+        $city_list = D('Area')->where($map)->field('id,cname1')->select();
+        $this->ajaxReturn($city_list);
+    }
+
+    public function getDistrictByCity($city_id){
+        $map['upid'] = $city_id;
+        $map['level'] = 3;
+
+        $district_list = D('Area')->where($map)->field('id,cname')->select();
+        $this->ajaxReturn($district_list);
+    }
+
 }
