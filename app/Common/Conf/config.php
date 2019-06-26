072a4823baa377a8a9a0fdec00ba6169e79459f6
tider <tider@quansitech.com>
2019-06-26 09:55:03 +0800

    t

--- app/Common/Conf/config.php
+++ app/Common/Conf/config.php
@@ -96,6 +96,23 @@ return array(
 		'callback' => false, //检测文件是否存在回调函数，如果存在返回文件信息数组
     ),
 
+    'UPLOAD_TYPE_AUDIO' => array(
+        'mimes'    => '', //允许上传的文件MiMe类型
+        'maxSize'  => 100*1024*1024, //上传的文件大小限制 (0-不做限制)
+        'exts'     => 'mp3,wav,cd,ogg,wma,asf,rm,real,ape,midi', //允许上传的文件后缀
+        'autoSub'  => true, //自动子目录保存文件
+        'subName'  => array('date','Ymd'), //子目录创建方式，[0]-函数名，[1]-参数，多个参数使用数组
+        'rootPath' => './Uploads/', //保存根路径
+        'savePath' => 'audio/', //保存路径
+        'saveName' => array('uniqid', ''), //上传文件命名规则，[0]-函数名，[1]-参数，多个参数使用数组
+        'saveExt'  => '', //文件保存后缀，空则使用原后缀
+        'replace'  => false, //存在同名是否覆盖
+        'hash'     => true, //是否生成hash编码
+        'callback' => false, //检测文件是否存在回调函数，如果存在返回文件信息数组
+        'oss_host' => 'https://****.oss-cn-beijing.aliyuncs.com',
+        'oss_meta' => array('Cache-Control' => 'max-age=2592000'),
+    ),
+
     /* 图片上传相关配置 */
     'UPLOAD_TYPE_IMAGE' => array(
 		'mimes'    => 'image/jpeg,image/png,image/gif,image/bmp', //允许上传的文件MiMe类型
@@ -110,7 +127,7 @@ return array(
 		'replace'  => false, //存在同名是否覆盖
 		'hash'     => true, //是否生成hash编码
 		'callback' => false, //检测文件是否存在回调函数，如果存在返回文件信息数组
-		'oss_host' => 'https://csh-pub-resp.oss-cn-shenzhen.aliyuncs.com',
+		'oss_host' => 'https://****.oss-cn-shenzhen.aliyuncs.com',
 		'oss_meta' => array('Cache-Control' => 'max-age=2592000'),
     ),
 
@@ -175,7 +192,7 @@ return array(
         'hash'     => true, //是否生成hash编码
         'callback' => false, //检测文件是否存在回调函数，如果存在返回文件信息数组
         'security' => true,
-        'oss_host' => 'https://csh-private-resp.oss-cn-shenzhen.aliyuncs.com'
+        'oss_host' => 'https://****.oss-cn-shenzhen.aliyuncs.com'
     ),
 
     'UPLOAD_TYPE_JOB_IMAGE' => array(
