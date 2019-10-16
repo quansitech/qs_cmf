2a2c6bf0dd350850924a8097739ffc437e496be7
xhiny <axhiny@163.com>
2019-10-16 19:09:01 +0800

    xh

--- app/Common/Conf/tags.php
+++ app/Common/Conf/tags.php
@@ -2,10 +2,9 @@
 
 return array(
 	'app_begin'    =>  array('Behaviors\\LoadDBConfigBehavior'),
-    'action_begin' =>  array('Behavior\CheckLangBehavior', 'Behaviors\\CheckThemeBehavior'),
+    'action_begin' =>  array('Behavior\CheckLangBehavior', 'Behaviors\\CheckThemeBehavior', 'Behaviors\\ResetRbacBehavior'),
     'view_filter'  =>  array('Behavior\\TokenBuildBehavior'),
     'app_init'=>array('Behaviors\\InitHookBehavior','Behaviors\\AppInitBehavior'),
     'template_filter'  =>  array('Behaviors\\TemplateSectionBehavior'),
     'after_home_instance' => array('Behaviors\\WxAutoLoginBehavior'),
-    'reset_rbac' => array('Behaviors\\ResetRbacBehavior')
 );
