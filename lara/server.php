17bb7bd9adacf24cede79deeac57d02e381449f4
tiderjian <tider@quansitech.com>
2020-04-21 12:28:30 +0800

    dusk测试修复

--- lara/server.php
+++ lara/server.php
@@ -9,6 +9,8 @@ $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
 
+$_SERVER['DUSK_TEST'] = true;
+
 // This file allows us to emulate Apache's "mod_rewrite" functionality from the
 // built-in PHP web server. This provides a convenient way to test a Laravel
 // application without having installed a "real" web server software here.
