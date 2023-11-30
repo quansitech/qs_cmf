944884b2204118d8203e8f08dc7783b16d34ee0f
Xhiny <axhiny@163.com>
2023-11-30 09:43:10 +0800

    xh

--- app/Common/Conf/Config/upload_config.php
+++ app/Common/Conf/Config/upload_config.php
@@ -8,28 +8,6 @@ return array(
     'UPLOAD_FILE_SIZE' => 50,
 
     /* 编辑器图片上传相关配置 */
-    'UPLOAD_TYPE_UEDITOR' => array(
-		'mimes'    => '', //允许上传的文件MiMe类型
-		'maxSize'  => 10*1024*1024, //上传的文件大小限制 (0-不做限制)
-		'exts'     => 'jpg,gif,png,jpeg', //允许上传的文件后缀
-		'autoSub'  => true, //自动子目录保存文件
-		'subName'  => array('date','Ymd'), //子目录创建方式，[0]-函数名，[1]-参数，多个参数使用数组
-		'rootPath' => './Uploads/editor/', //保存根路径
-		'savePath' => '', //保存路径
-		'saveName' => array('uniqid', ''), //上传文件命名规则，[0]-函数名，[1]-参数，多个参数使用数组
-		'saveExt'  => '', //文件保存后缀，空则使用原后缀
-		'replace'  => false, //存在同名是否覆盖
-		'hash'     => true, //是否生成hash编码
-		'callback' => false, //检测文件是否存在回调函数，如果存在返回文件信息数组
-        'oss_host' => env("ALIOSS_HOST"),
-        'os_upload_meta' => array('Cache-Control' => 'max-age=2592001'),
-        'tos_host' => env('VOLC_HOST'),
-//        'vendorType' => 'volcengine_tos',
-        'cos_host' => env('COS_HOST'),
-//        'upload_tos_host' => env('VOLC_HOST'),
-    ),
-
-    /* 编辑器图片上传相关配置 */
     'UPLOAD_TYPE_EDITOR' => array(
 		'mimes'    => '', //允许上传的文件MiMe类型
 		'maxSize'  => 10*1024*1024, //上传的文件大小限制 (0-不做限制)
@@ -44,7 +22,7 @@ return array(
 		'hash'     => true, //是否生成hash编码
 		'callback' => false, //检测文件是否存在回调函数，如果存在返回文件信息数组
         'oss_host' => env("ALIOSS_HOST"),
-        'os_upload_meta' => array('Cache-Control' => 'max-age=2592001'),
+        'os_upload_meta' => array('Cache-Control' => 'max-age=2592000'),
         'tos_host' => env('VOLC_HOST'),
 //        'vendorType' => 'volcengine_tos',
         'cos_host' => env('COS_HOST'),
@@ -65,7 +43,7 @@ return array(
         'hash'     => true, //是否生成hash编码
         'callback' => false, //检测文件是否存在回调函数，如果存在返回文件信息数组
         'oss_host' => env("ALIOSS_HOST"),
-        'oss_meta' => array('Cache-Control' => 'max-age=2592001'),
+        'oss_meta' => array('Cache-Control' => 'max-age=2592000'),
     ),
 
     /* 图片上传相关配置 */
@@ -83,11 +61,7 @@ return array(
 		'hash'     => true, //是否生成hash编码
 		'callback' => false, //检测文件是否存在回调函数，如果存在返回文件信息数组
 		'oss_host' => env("ALIOSS_HOST"),
-		'oss_meta' => array('Cache-Control' => 'max-age=2592001'),
-        'os_upload_meta' => array('Cache-Control' => 'max-age=2592001', 'Content-Disposition' => 'attachment;filename=__title__'),
-        'tos_host' => env('VOLC_HOST'),
-//        'vendorType' => 'volcengine_tos',
-        'cos_host' => env('COS_HOST'),
+        'oss_meta' => array('Cache-Control' => 'max-age=2592000'),
     ),
 
     'UPLOAD_TYPE_VIDEO' => array(
