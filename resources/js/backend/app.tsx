b96df37a514c81605380a7ee11bf478def895dbe
jack <958691165@qq.com>
2024-10-15 17:06:22 +0800

    完成antd渲染

--- resources/js/backend/app.tsx
+++ resources/js/backend/app.tsx
@@ -1,12 +1,31 @@
-import {createInertiaApp} from '@inertiajs/react'
-import {createRoot} from 'react-dom/client'
+import {createInertiaApp, router} from '@inertiajs/react';
+import {createRoot} from 'react-dom/client';
+import Layout from "@quansitech/antd-admin/components/Layout";
+import {ReactNode} from "react";
+import "@quansitech/antd-admin/lib/container";
+import {App as AntdApp} from "antd";
+
+router.on('invalid', ev => {
+    // 返回html时直接跳转
+    if (ev.detail.response.headers["content-type"].includes("text/html")) {
+        ev.preventDefault()
+        window.location.href = ev.detail.response.config.url as string
+    }
+})
 
 createInertiaApp({
-    resolve: name => {
+    resolve: async name => {
         const pages = import.meta.glob('./Pages/**/*.tsx')
-        return pages[`./Pages/${name}.tsx`]()
+        const page: any = await pages[`./Pages/${name}.tsx`]()
+        page.default.layout = page.default.layout || ((page: ReactNode) => <Layout children={page}/>)
+
+        return page
     },
     setup({el, App, props}) {
-        createRoot(el).render(<App {...props} />)
+        createRoot(el).render(
+            <AntdApp>
+                <App {...props} />
+            </AntdApp>
+        )
     },
 })
--- vite.backend.config.js
+++ vite.backend.config.js
@@ -1,10 +1,29 @@
 import {defineConfig} from "vite";
 import laravel from "laravel-vite-plugin";
 import react from '@vitejs/plugin-react';
+import {visualizer} from 'rollup-plugin-visualizer'
 
 export default defineConfig(() => {
     return {
         base: '/Public/backend/build',
+        build: {
+            sourcemap: true,
+            rollupOptions: {
+                plugins: [visualizer()],
+                output: {
+                    manualChunks: {
+                        react: ['react', 'react-dom'],
+                        antd: ['antd', '@ant-design/icons'],
+                        axios: ['axios'],
+                        'pro-components': ['@ant-design/pro-components'],
+                        'pro-layout': ['@ant-design/pro-layout'],
+                        'pro-table': ['@ant-design/pro-table'],
+                        'pro-form': ['@ant-design/pro-form'],
+                        'pro-field': ['@ant-design/pro-field'],
+                    }
+                }
+            }
+        },
         plugins: [
             laravel({
                 input: ['resources/js/backend/app.tsx'],
@@ -18,7 +37,12 @@ export default defineConfig(() => {
             react(),
         ],
         css: {
-            modules: true
+            modules: true,
+            preprocessorOptions: {
+                scss: {
+                    api: 'modern-compiler',
+                }
+            }
         },
         server: {
             port: 5183
