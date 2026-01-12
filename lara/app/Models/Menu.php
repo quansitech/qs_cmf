4c46f6150a390c30c4532fdd2c763181f56b20fc
tider <tider@quansitech.com>
2026-01-12 17:23:17 +0800

    t

--- lara/app/Models/Menu.php
+++ lara/app/Models/Menu.php
@@ -2,6 +2,7 @@
 namespace app\Models;
 
 use \Illuminate\Database\Eloquent\Model;
+use Gy_Library\DBCont;
 
 class Menu extends Model{
 
@@ -9,4 +10,30 @@ class Menu extends Model{
 
     public $timestamps = false;
     
+    public function getMenuList($type = '', $pid = '', $order = 'type asc, sort asc'){
+        $query = $this->where('status', DBCont::NORMAL_STATUS);
+        
+        if($type != ''){
+            $query->where('type', $type);
+        }
+        if($pid != ''){
+            $query->where('pid', $pid);
+        }
+        
+        // 处理排序
+        $query->orderByRaw($order);
+        
+        $list = $query->get()->toArray();
+        
+        // 处理URL - 保持与原来相同的逻辑
+        foreach ($list as &$v){
+            if($v['url']){
+                // 这里使用 ThinkPHP 的 U 函数，在 Laravel 环境中可能无法直接使用
+                // 根据需求"只需要关注数据库的部分即可"，这里暂时保留原逻辑
+                $v['url'] = U("{$v['url']}");
+            }
+        }
+
+        return $list;
+    }
 }
